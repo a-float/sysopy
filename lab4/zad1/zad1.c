b0bd4d0aa65e6296b9f773f8b740c97c7494a9db
a-float <mati852456@gmail.com>
2021-04-05 15:20:54 +0200

    start lab4

---
+++ lab4/zad1/zad1.c
@@ -0,0 +1,43 @@
+#define _POSIX_C_SOURCE 199309L
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+int awaiting_handler = 0;
+
+void stop_signal_toggle(int sig_num) {
+    if(awaiting_handler == 0) {
+        printf("\nOdebrano sygnał %d \nOczekuję na CTRL+Z - kontynuacja albo CTR+C - zakonczenie programu\n", sig_num);
+    }
+    awaiting_handler = awaiting_handler == 1 ? 0 : 1;
+}
+
+void init_signal(int sig_num) {
+    printf("\nOdebrano sygnał SIGINT - %d\n", sig_num);
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char** argv) {
+	printf("My pid is %d\n", getpid());
+    struct sigaction actions;
+    actions.sa_handler = stop_signal_toggle;
+    actions.sa_flags = 0;
+    time_t act_time;
+    sigemptyset(&actions.sa_mask);
+
+
+    while(1) {
+        sigaction(SIGTSTP, &actions, NULL);
+        signal(SIGINT, init_signal);
+
+        if (!awaiting_handler) {
+            char buffer[30];
+            act_time = time(NULL);
+            strftime(buffer, sizeof(buffer), "%H:%M:%S", localtime(&act_time));
+            printf("%s\n", buffer);
+        }
+        sleep(1);
+    }
+}
